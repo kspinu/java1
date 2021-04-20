38a3331e609694146ae7ee0c4b3538d71cd14565
Kamelia Spinu <cameliaspinu.123@gmail.com>
2021-04-20 22:57:55 +0300

    commit 1

--- src/main/java/testgit/Test1.java
+++ src/main/java/testgit/Test1.java
@@ -1,4 +1,21 @@
 package testgit;
 
+import java.util.Arrays;
+import java.util.Collections;
+
 public class Test1 {
+    public static void main(String[] args) {
+        int[] arr = {8,9,10,1,2,3,4,5};
+        Arrays.sort(arr);
+
+        System.out.println(Arrays.toString(arr));
+
+
+        int[] arrDesc = Arrays.stream(arr).boxed()
+                .sorted(Collections.reverseOrder())
+                .mapToInt(Integer::intValue)
+                .toArray();
+
+        System.out.println(Arrays.toString(arrDesc));
+    }
 }
